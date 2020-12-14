9464fba9cbc7acdd5a9d949af831b8fd03d4e0ce
unknown <dejanb06@gmail.com>
2020-12-15 00:09:16 +0100

    Initial commit.

---
+++ 01_functions/load_data.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+
+df <- read.csv("00_data/2020 November General Election - Turnout Rates.csv")
+
+
+
---
+++ 02_eda/EDA.R
@@ -0,0 +1,11 @@
+library(SmartEDA)
+source('01_functions/load_data.R')
+
+
+# Overview of the data - Type = 1
+ExpData(data=df,type=1)
+
+# Structure of the data - Type = 2
+ExpData(data=df,type=2)
+
+
